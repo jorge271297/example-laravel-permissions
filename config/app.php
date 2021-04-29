c70e0111dfb345611da6e323bafeebb61d33de33
Jorge <jormope271297@gmail.com>
2021-04-29 17:24:14 -0500

    spatie/laravel-permission v2 package installation

--- config/app.php
+++ config/app.php
@@ -177,6 +177,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //spatie/laravel-permission
+        Spatie\Permission\PermissionServiceProvider::class,
     ],
 
     /*
